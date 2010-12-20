4326614c0b32794c9b833dc5ea6fc7788ea6497a
wiseleyb <wiseleyb@rasputin.local>
2010-12-19 21:01:41 -0800

    first commit

---
+++ config/initializers/constants.rb
@@ -0,0 +1,14 @@
+APP_NAME = "App Name"
+
+# case Rails.env
+# when "development"
+#   REDIS = Redis.new(:port => 6390)
+#   BASE_URL = "http://localhost:3000"
+# when "test"
+#   REDIS = Redis.new(:port => 6391)
+#   BASE_URL = "http://localhost:3000"
+# when "production"
+#   REDIS = Redis.new(:port => 6392)
+#   BASE_URL = "http://yoururl.com"
+# end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenRentals::Application.config.secret_token = '339daf4a1e534aba957eff6f9b3d03b7b52f417ccb303a178f15b1a579fb709eead0ab07d5edf2c78b830f095f957dc875f0b46062368769ee1c6effbbafafe5'
